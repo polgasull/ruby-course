da040ee5a9199ca7fc7fbe50d42d4c3edf3bf3ad
polgasull <pol@innroute.com>
2018-09-10 09:31:55 +0200

    functions with self

--- Prework/functions/functions.rb
+++ Prework/functions/functions.rb
@@ -1,3 +1,37 @@
-def say_hello
-  puts "Hello!"
+class FunctionClass
+
+# For running on command line we need self
+# then we have to write ruby -r "./filename.rb" -e "puts NameClass.function_name"
+
+  def self.say_hello
+    puts "Hello!"
+  end
+
+  def self.say_hello(name)
+    puts "Hello, #{name}"
+  end
+
+  say_hello("Rafa")
+  say_hello("Xavi")
+  say_hello("Isra")
+  say_hello("James" + "Bond")
+
+  def self.add(a, b)
+    puts a + b
+  end
+
+  add((5 * 5), (5 + 3))
+  add((3 + 4) * 10, 8 - 4 / 2)
+
+  def self.taste(food)
+    if food == "bacon"
+      puts "Yummy!! BACON!!"
+    else
+      puts "Yummy!! SPINACH!!"
+    end
+  end
+
+  taste "spinach"
+  taste "bacon"
+
 end
